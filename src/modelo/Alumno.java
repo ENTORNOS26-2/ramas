d91e585d046404d72e71ae046e22054f2addb449
Jesús Alcocer Fernández <jesusalcocerfdez@cdpjosecabrera.es>
2026-02-03 12:53:04 +0100

    agregamos la clase alumno

---
+++ src/modelo/Alumno.java
@@ -0,0 +1,20 @@
+package modelo;
+
+public class Alumno {
+    String nombre;
+    int edad;
+
+    public Alumno(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }   
+    
+    public String getNombre() {
+        return nombre;
+    }   
+    public int getEdad() {
+        return edad;
+    }
+        
+
+}
